7750f23a1a37805401f7cb2665068ea36e3f1275
Sandra Laban <sandra.muncan@gmail.com>
2023-10-06 15:19:22 +1300

    addind drums project

---
+++ drums/js/drum-kit.js
@@ -0,0 +1,17 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  console.log(audio)
+  if (!audio) return // stop the function from running all together
+  audio.currentTime = 0 // rewind to the start
+  audio.play()
+  key.classList.add('playing')
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return // skip it if it's not transformed
+  this.classList.remove('playing')
+}
+const keys = document.querySelectorAll('.key')
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound)
